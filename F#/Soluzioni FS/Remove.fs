e9af472e2a0546ff22e58d1c11f3d8066d09281c
Lorenzo Vaccarecci <gekkeijuita@gmail.com>
2024-06-20 11:54:29 +0200

    f# ultimato

--- F#/Soluzioni FS/Remove.fs
+++ F#/Soluzioni FS/Remove.fs
@@ -11,3 +11,17 @@ let accRemove i =
         | _ -> List.rev acc
 
     loop [] i
+
+let rec remove l i =
+    match i,l with
+    | i, _ when i < 0 || i >= List.length l -> l
+    | 0, _ :: tl -> tl
+    | _, hd :: tl -> hd :: remove tl (i - 1)
+
+let removeAcc l i =
+    if i < 0 || i >= List.length l then l
+    else
+        let rec loop l i acc =
+        match i,l with
+        | 0, _ :: tl -> acc @ tl
+        | _, hd :: tl -> loop tl (i - 1) (hd :: acc)
